51124636ed84c7b2e82e28b883b25e6e12825569
Alexander Ding <ading6@illinois.edu>
2020-05-07 17:32:22 -0500

    Resovled merge conflict and added config file error handling 2

--- container.c
+++ container.c
@@ -392,6 +392,7 @@ int main(int argc, char** argv) {
           return EXIT_FAILURE;
         }
         token = strtok(NULL, "\n");
+	  }
       }
     }
     // Determines what new namespaces we will create for our containerized process.
