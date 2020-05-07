694c255c04e32c0b5d9578c6ea890987d2a3ba6b
Alexander Ding <ading6@illinois.edu>
2020-05-07 16:57:21 -0500

    fixed file parsing stuff

--- container.c
+++ container.c
@@ -361,21 +361,24 @@ int main(int argc, char** argv) {
 
     printf("Opening and reading config file...\n");
     char* pointer = NULL;
-    if((pointer = strstr(buff, "mem_limit:")) != NULL){
-      printf("Changing mem_limit to:%s", pointer+12);
-      options.mem_limit = pointer+12;
+	char* token = NULL;
+	token = strtok(buff, "\n");
+	while(token != NULL){
+    if((pointer = strstr(token, "mem_limit:")) != NULL){
+      printf("Changing mem_limit to:%s\n", pointer+11);
+      options.mem_limit = pointer+11;
     }
-    if((pointer = strstr(buff, "mem_plus_swap_limit:")) != NULL){
-      printf("Changing mem_plus_swap_limit to:%s", pointer+21);
+    if((pointer = strstr(token, "mem_plus_swap_limit:")) != NULL){
+      printf("Changing mem_plus_swap_limit to:%s\n", pointer+21);
       options.mem_plus_swap_limit = pointer+21;
     }
-    if((pointer = strstr(buff, "pid_limit:")) != NULL){
-      printf("Changing pid_limit to:%s", pointer+11);
+    if((pointer = strstr(token, "pid_limit:")) != NULL){
+      printf("Changing pid_limit to:%s\n", pointer+11);
       options.pid_limit = pointer+11;
     }
-    if((pointer = strstr(buff, "CPU%:")) != NULL){
-      printf("Changing CPU: %s\n", pointer+5);
-      int new_quota = atoi(pointer+5)*10000;
+    if((pointer = strstr(token, "CPU%:")) != NULL){
+      printf("Changing CPU:%s\n", pointer+6);
+      int new_quota = atoi(pointer+6)*10000;
       if(new_quota < 0){
         printf("Cannot change CPU usage to negative amount...and why would you?\n");
         return EXIT_FAILURE;}
@@ -386,7 +389,8 @@ int main(int argc, char** argv) {
         char quota = (char) new_quota;
         options.cpu_quota = &quota;
       }
-
+	token = strtok(NULL, "\n");
+	}
     }
     // Determines what new namespaces we will create for our containerized process.
     // Note, NEWIPC is going to be set from within that process since we need to synchronize over cgroups_done.
